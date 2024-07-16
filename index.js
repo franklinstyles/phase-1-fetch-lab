62ffd0c981392529534f6a8496d0b2c018dfdd85
franklin ndegwa <franklin.ndegwa@student.moringaschool.com>
2024-07-16 19:50:07 +0300

    changes to js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+  return fetch( "https://anapioficeandfire.com/api/books")
+  .then(function(response){
+    return response.json();
+  })
+  .then (function(json){
+    return renderBooks(json)
+  })
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -11,7 +19,6 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
